a78cbe8e75cdfe2c758334423f85ebdd1ab432f4
Bahtier Kamalov <bahtierkamalov21@gmail.com>
2022-12-17 19:38:46 +0600

    backup

--- Main.py
+++ Main.py
@@ -2,14 +2,18 @@ import tkinter as tk
 from tkinter import ttk
 import pandas as pd
 from tkinter import *
+import matplotlib
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 
 class Main(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Визуализация Python")
+        self.iconbitmap("./src/assets/favicon.ico")
         w = self.winfo_screenwidth()
         h = self.winfo_screenheight()
         w = w - 100
@@ -29,7 +33,7 @@ class FrameDrawer(ttk.Frame):
         
         # Описание фрейма
         self['height'] = self.winfo_screenheight()
-        self['width'] = (self.winfo_screenwidth() // 2) - 200
+        self['width'] = (self.winfo_screenwidth() // 2) - 180
         self['padding'] = 5
         self['borderwidth'] = 1
         self['relief'] = 'raised'
@@ -37,24 +41,28 @@ class FrameDrawer(ttk.Frame):
         # Упаковка
         self.grid(row=0, column=0)
         
-        # Данные для графиков
-        self.list = [[5, 4, 5, 5, 5], [4, 5, 3, 3, 3], [3, 5, 2, 2, 2], [5, 5, 5, 5, 5], [2, 5, 4, 4, 4]]
-        self.index = ["Математика", "Казахски", "Английский", "Русский", "Физика"]
-        self.columns = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"]
-        table = pd.DataFrame(self.list, index=self.index, columns=self.columns)
+        # Данные
+        self.data = pd.DataFrame({"Математика": [5, 4, 4, 5, 5], "Русский язык": [4, 4, 4, 4, 4]})
         
-        # Графики
-        fig, ax = plt.subplots()
+        # Таблицы
+        self.fig, self.ax = plt.subplots(figsize=(4.8, 5), layout='constrained')
+        self.ax.set_title("График в виде Plot")
+        self.ax.set_xlabel('Предметы')  # Add an x-label to the axes.
+        self.ax.set_ylabel('Оценки')  # Add a y-label to the axes.
+        self.ax.plot(["Математика", "Русский язык", "Казахский язык", "Информатика"], [[5, 5, 5, 5, 4], [4, 5, 4, 5, 5], [3, 4, 5, 5, 3], [4, 5, 4, 5, 5]]);  # Plot some data on the axes.
+        canvas = FigureCanvasTkAgg(self.fig, self)
+        canvas.draw()
+        canvas.get_tk_widget().place(y=80, x=5) 
         
-        title = plt.title("Линейный график")
-        line = ax.plot(table)
-        
-        self.canvas = FigureCanvasTkAgg(fig, self)
-        self.canvas.draw()
-        self.canvas.get_tk_widget().place(y=100)
-        
-        self.label = Label(self, text="Моя статистика за неделю", bg="#333", padx=20, pady=10, font=16, fg="white", width=self.winfo_reqwidth() - 440)
+        # Компоненты
+        self.label = Label(self, text="Моя статистика за неделю", bg="#333", padx=20, pady=10, font=16, fg="white", width=self.winfo_reqwidth() - 458)
         self.label.place(x=20, y=20)
+        
+        def OpenCanvasTableInNotebook():
+            ptk.show()
+        
+        self.buttonCheckInNotebook = Button(self, width=self.winfo_reqwidth() - 458, padx=20, pady=10, text="Открыть график в графическом окне matplotlib", font=16, fg="white", bg="#333", command=OpenCanvasTableInNotebook)
+        self.buttonCheckInNotebook.place(y=self.winfo_reqheight() - 170, x=20)
         
 
 if __name__ == "__main__":
